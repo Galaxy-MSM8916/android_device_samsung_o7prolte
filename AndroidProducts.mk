d2a7926be3dc7f3057be9a5c0518c2d5e5b3b86e
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-09-24 20:54:01 -0400

    o7prolte: Update AndroidProducts makefile
    Consider {lineage,rr,omni,pa}_o7prolte the AOSP way
    
    Change-Id: I06bc2db295d530fb6b4ae4a67e80225f449082b2

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,5 @@
 LOCAL_PATH := device/samsung/o7prolte
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_o7prolte.mk
+	$(LOCAL_DIR)/omni_o7prolte.mk \
+	$(LOCAL_DIR)/rr_o7prolte.mk \
+	$(LOCAL_DIR)/lineage_o7prolte.mk
