b4483150017c89548d74d9aec9d6ec4c0ec44654
Dan Willemsen <dwillemsen@google.com>
2020-06-15 20:37:23 +0800

    o7prolte: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: I81bec671b400d537ad508057287cb0870cd40541

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,3 +3,8 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/omni_o7prolte.mk \
 	$(LOCAL_DIR)/rr_o7prolte.mk \
 	$(LOCAL_DIR)/lineage_o7prolte.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_o7prolte-eng \
+    lineage_o7prolte-userdebug \
+    lineage_o7prolte-user
